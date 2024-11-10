aea4d80497658976976aa4a9972e8a2b765dc7b1
Tomas Angelo <krixnliza@ya.ru>
2024-11-10 18:23:29 +0600

    Initial backend setup with Poem

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use poem::{get, handler, listener::TcpListener, Route, Server};
+
+#[handler]
+fn hello() -> String {
+    format!("hello from there!")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let app = Route::new().at("/", get(hello));
+    Server::new(TcpListener::bind("0.0.0.0:8080"))
+        .run(app)
+        .await
 }
