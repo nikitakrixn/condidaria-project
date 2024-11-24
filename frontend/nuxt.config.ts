03641439188350fd1d6f5b5e25be7d15e84741f4
Tomas Angelo <krixnliza@ya.ru>
2024-11-24 14:35:20 +0600

    Change default port

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      backendUrl: process.env.BACKEND_URL || 'http://localhost:3000/api',
+      backendUrl: process.env.BACKEND_URL || 'http://localhost:8000/api',
     },
   },
 })
